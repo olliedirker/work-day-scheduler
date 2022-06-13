1ee47cd16109e9015cee1532118666fc0831bc5e
olliedirker <olliedirker@gmail.com>
2022-06-13 16:46:13 -0500

    finished html rows on html

--- Develop/assets/script.js
+++ Develop/assets/script.js
@@ -22,6 +22,24 @@ $(document).ready(function () {
         })
 
     }
-    $(this).siblings(".description").val()
+    //on save btn send to local storage
+    $(".saveBtn").on("click", function () {
+        $(this).siblings(".description").val()
+        var timeBlock = $(this).closest(".row").attr("id");
+
+        localStorage.setItem(timeBlock, planLog);
+    })
+    //run through each row and retrieve the local storage
+    $(".row").each(function() {
+        var hour = $(this).attr("id");
+        var planInput = localStorage.getItem(hour);
+    
+        if(!planInput) {
+          return;
+        } else {
+          $(this).children(".description").val(planInput);
+        };
+      })
+
     timeShade()
 });
